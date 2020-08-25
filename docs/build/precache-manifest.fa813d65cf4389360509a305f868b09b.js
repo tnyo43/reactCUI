a6ace87a0b7b0f9d390ff22870bbd6cb91fddaaa
tyno43 <tnyo43@gmail.com>
2020-08-26 02:51:17 +0900

    master push: vim implemented version

---
+++ docs/build/precache-manifest.fa813d65cf4389360509a305f868b09b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f18f795adefd90067e0c8988a25b99a",
+    "url": "/reactCUI/index.html"
+  },
+  {
+    "revision": "aafae40b3d480168bd4e",
+    "url": "/reactCUI/static/css/main.721a9933.chunk.css"
+  },
+  {
+    "revision": "e55930cf839af201b672",
+    "url": "/reactCUI/static/js/2.453ce14b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCUI/static/js/2.453ce14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafae40b3d480168bd4e",
+    "url": "/reactCUI/static/js/main.380d9dcc.chunk.js"
+  },
+  {
+    "revision": "d38520e150ca8a2315a1",
+    "url": "/reactCUI/static/js/runtime-main.910fa417.js"
+  }
+]);
